3fe242ff7a638434fcab36c75ada259d6b086b06
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-11-17 20:41:32 +0100

    Update quickstart.js

--- examples/quickstart.js
+++ examples/quickstart.js
@@ -14,7 +14,7 @@ try {
 
 const accessToken = (() => {
   if (process.argv.length !== 3) {
-    console.log('usage: node examples/quickstart.js VFD7X3EUFNWFFP7XAMRB2YJLQTHCDAUI');
+    console.log('usage: node examples/quickstart.js <VFD7X3EUFNWFFP7XAMRB2YJLQTHCDAUI>');
     process.exit(1);
   }
   return process.argv[2];
